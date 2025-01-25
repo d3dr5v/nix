7656be609091b584940772d861377dc9bb4db250
David Roussov <david.roussov@protonmail.com>
2025-01-25 23:12:52 +1100

    update config

--- flake.nix
+++ flake.nix
@@ -1,13 +1,21 @@
 {
-  description = "Development environment";
+  description = "Global development environment";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs }: rec {
-    devShells.aarch64-darwin.default = nixpkgs.lib.mkShell {
-      buildInputs = with nixpkgs.legacyPackages.aarch64-darwin; [
+  outputs = { self, nixpkgs }:
+  let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        allowUnfreePredicate = _: true;
+      };
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
         git
         neovim
         tree
@@ -25,13 +33,28 @@
         neomutt
         sc-im
         tshark
-        pandoc
-        texlive.combined.scheme-full
         kubectl
+        pandoc
+#texlive.combined.scheme-full
       ];
 
       shellHook = ''
+        if [ -z "$OPENAI_API_KEY" ]; then
+          echo "OPENAI_API_KEY is not set. Retrieving from AWS Secrets Manager..."
+
+          SECRET_VALUE=$(AWS_PROFILE=davidroussov aws secretsmanager get-secret-value --secret-id "openai.api_key" --query 'SecretString' --output text 2>/dev/null)
+
+          if [ $? -ne 0 ]; then
+              exit 1
+          fi
+
+          export OPENAI_API_KEY="$SECRET_VALUE"
+        fi
+
+        
         cd /Users/david/@/
+
+        /bin/zsh
       '';
     };
   };
