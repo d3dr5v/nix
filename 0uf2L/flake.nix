bee5bc139570ad98b9dad20c5f2cab995ca31b4e
David Roussov <david.roussov@gmail.com>
2026-07-12 10:06:10 +1000

    add new flakes

---
+++ 0uf2L/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/ace249efcbdfbd4c53aa9529251ab848fe68c5e2";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        oci-cli
+        yarn
+      ];
+    };
+  };
+}
--- 1GKm3/flake.nix
+++ 1GKm3/flake.nix
@@ -66,7 +66,6 @@
         tig
         fzf
         mpv
-        supabase-cli
         postgresql
       ];
 
